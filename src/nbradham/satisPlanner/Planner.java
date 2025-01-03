3314eca984be45b4aed67fc8756095f2f14a18a8
nickolasbradham <nickolasbradham@gmail.com>
2025-01-03 12:05:22 -0700

    Better GUI.

--- src/nbradham/satisPlanner/Planner.java
+++ src/nbradham/satisPlanner/Planner.java
@@ -1,8 +1,11 @@
 package nbradham.satisPlanner;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Toolkit;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -10,14 +13,18 @@ import java.util.LinkedList;
 import java.util.Map.Entry;
 import java.util.Queue;
 import java.util.Scanner;
-
+import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingUtilities;
+import javax.swing.border.TitledBorder;
 
 final class Planner {
 
@@ -34,83 +41,160 @@ final class Planner {
 	}
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(Planner.class.getResourceAsStream("/best.tsv")).useDelimiter(DELIM);
-		scan.nextLine();
-		HashMap<String, Recipe> recipes = new HashMap<>();
-		while (scan.hasNextLine())
-			recipes.put(scan.next(),
-					new Recipe(scan.next(), parseList(scan.next()), scan.next(), parseList(scan.next())));
-		scan.close();
-		JPanel pane = new JPanel(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.gridy = gbc.gridx = 0;
-		gbc.anchor = GridBagConstraints.BASELINE_TRAILING;
-		pane.add(new JLabel("Item: "), gbc);
-		gbc.gridy = 1;
-		pane.add(new JLabel("Rate (items/min): "), gbc);
-		gbc.gridx = 1;
-		gbc.anchor = GridBagConstraints.BASELINE_LEADING;
-		JSpinner spin = new JSpinner(new SpinnerNumberModel(1, 0, Float.MAX_VALUE, .1));
-		spin.setPreferredSize(new Dimension(60, spin.getPreferredSize().height));
-		pane.add(spin, gbc);
-		gbc.gridy = 0;
-		String[] options = recipes.keySet().toArray(new String[recipes.size()]);
-		Arrays.sort(options);
-		JComboBox<String> itmSel = new JComboBox<>(options);
-		pane.add(itmSel, gbc);
-		JOptionPane.showMessageDialog(null, pane, "What is the desired item and rate?", JOptionPane.QUESTION_MESSAGE);
-		scan = new Scanner(Planner.class.getResourceAsStream("/raws.tsv")).useDelimiter("\r*\n");
-		HashSet<String> raws = new HashSet<>();
-		scan.forEachRemaining(s -> raws.add(s));
-		System.out.printf("Raws: %s%n", raws);
-		scan.close();
-		HashMap<String, Double> needs = new HashMap<>();
-		HashMap<String, ProductionStep> steps = new HashMap<>();
-		Queue<String> queue = new LinkedList<>();
-		HashSet<String> queueSet = new HashSet<>();
-		String item = (String) itmSel.getSelectedItem();
-		needs.put(item, (double) spin.getValue());
-		queue.offer(item);
-		queueSet.add(item);
-		while ((item = queue.poll()) != null) {
-			queueSet.remove(item);
-			System.out.printf("Getting recipe for %s...%n", item);
-			Recipe rec = recipes.get(item);
-			double adj = Math.ceil(needs.remove(item) * ROUND_MAG / rec.outs.get(item)) / ROUND_MAG;
-			System.out.printf("Adjustment: %f%n", adj);
-			if (Math.abs(adj) > .000001) {
-				ProductionStep step = steps.get(item);
-				if (step == null)
-					steps.put(item, new ProductionStep(rec, adj));
-				else
-					step.count += adj;
-				for (Entry<String, Float> e : rec.ins.entrySet()) {
-					String mod = e.getKey();
-					if (!mod.equals(item)) {
-						if (!raws.contains(mod) && queueSet.add(mod))
-							queue.offer(mod);
-						needs.merge(mod, (e.getValue() * adj), (a, b) -> a + b);
+		SwingUtilities.invokeLater(() -> {
+			Scanner scan = new Scanner(Planner.class.getResourceAsStream("/raws.tsv")).useDelimiter("\r*\n");
+			HashSet<String> raws = new HashSet<>();
+			scan.forEachRemaining(s -> raws.add(s));
+			scan.close();
+			scan = new Scanner(Planner.class.getResourceAsStream("/recipes.tsv")).useDelimiter(DELIM);
+			scan.nextLine();
+			HashMap<String, ArrayList<Recipe>> recipesByOut = new HashMap<>();
+			while (scan.hasNextLine()) {
+				Recipe recipe = new Recipe(scan.next(), parseList(scan.next()), scan.next(), parseList(scan.next()));
+				recipe.outs.keySet().forEach(o -> {
+					if (!raws.contains(o)) {
+						ArrayList<Recipe> recipes = recipesByOut.get(o);
+						if (recipes == null)
+							recipesByOut.put(o, recipes = new ArrayList<>());
+						recipes.add(recipe);
 					}
-				}
+				});
 			}
-		}
-		StringBuilder sb = new StringBuilder();
-		sb.append("Raw Inputs:\n");
-		needs.forEach((k, v) -> sb.append(String.format("%s: %f%n", k, v)));
-		sb.append("\nProduction Steps:\nMachine%\tMachine\tRecipe\n");
-		steps.values().forEach(s -> {
-			sb.append(String.format("%.4f%%\t%s\t", s.count * 100, s.recipe.machine));
-			if (!s.recipe.name.isBlank())
-				sb.append(String.format("%s\t", s.recipe.name));
-			sb.append(String.format("(%s > %s)%n", s.recipe.ins.keySet(), s.recipe.outs.keySet()));
+			scan.close();
+			JPanel recipeSelectPane = new JPanel(new GridBagLayout());
+			GridBagConstraints gbc = new GridBagConstraints();
+			HashMap<String, JComboBox<Recipe>> comboByItem = new HashMap<>();
+			recipesByOut.forEach((k, v) -> {
+				Recipe[] arr = v.toArray(new Recipe[v.size()]);
+				Arrays.sort(arr, (a, b) -> a.name.compareTo(b.name));
+				comboByItem.put(k, new JComboBox<>(arr));
+			});
+			String[] items = comboByItem.keySet().toArray(new String[comboByItem.size()]);
+			Arrays.sort(items);
+			gbc.gridy = 0;
+			gbc.anchor = GridBagConstraints.BASELINE_LEADING;
+			for (String i : items) {
+				gbc.gridx = 0;
+				++gbc.gridy;
+				recipeSelectPane.add(new JLabel(i), gbc);
+				gbc.gridx = 1;
+				recipeSelectPane.add(comboByItem.get(i), gbc);
+			}
+			JScrollPane scroll = new JScrollPane(recipeSelectPane, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+					JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+			scroll.getVerticalScrollBar().setUnitIncrement(20);
+			scroll.setBorder(new TitledBorder("Recipe Selection"));
+			JSpinner spin = new JSpinner(new SpinnerNumberModel(1, 0, Float.MAX_VALUE, .1));
+			spin.setPreferredSize(new Dimension(60, spin.getPreferredSize().height));
+			String[] options = recipesByOut.keySet().toArray(new String[recipesByOut.size()]);
+			Arrays.sort(options);
+			JComboBox<String> itmSel = new JComboBox<>(options);
+			JTextArea out = new JTextArea();
+			out.setEditable(false);
+			JFrame frame = new JFrame("Production Planner");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setLayout(new BorderLayout());
+			JButton calculate = new JButton("Calculate");
+			calculate.addActionListener(event -> {
+				HashMap<String, Double> needs = new HashMap<>();
+				HashMap<String, ProductionStep> steps = new HashMap<>();
+				Queue<String> queue = new LinkedList<>();
+				HashSet<String> queueSet = new HashSet<>();
+				String item = (String) itmSel.getSelectedItem();
+				needs.put(item, (double) spin.getValue());
+				queue.offer(item);
+				queueSet.add(item);
+				byte n = Byte.MIN_VALUE;
+				while ((item = queue.poll()) != null) {
+					queueSet.remove(item);
+					System.out.printf("Getting recipe for %s...%n", item);
+					Recipe rec = (Recipe) comboByItem.get(item).getSelectedItem();
+					double adj = Math.ceil(needs.remove(item) * ROUND_MAG / rec.outs.get(item)) / ROUND_MAG;
+					System.out.printf("Adjustment: %f%n", adj);
+					if (Math.abs(adj) > .000001) {
+						ProductionStep step = steps.get(item);
+						if (step == null)
+							steps.put(item, new ProductionStep(rec, adj));
+						else
+							step.count += adj;
+						for (Entry<String, Float> e : rec.ins.entrySet()) {
+							String mod = e.getKey();
+							if (!mod.equals(item)) {
+								if (!raws.contains(mod) && queueSet.add(mod))
+									queue.offer(mod);
+								needs.merge(mod, (e.getValue() * adj), (a, b) -> a + b);
+							}
+						}
+					}
+					if (++n == Byte.MAX_VALUE)
+						if (JOptionPane.showConfirmDialog(frame,
+								"Possibly caught in an infinite loop. Check your recipe selections.\nForce stop?",
+								"Warning", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
+							queue.clear();
+						else
+							n = Byte.MIN_VALUE;
+				}
+				StringBuilder sb = new StringBuilder();
+				sb.append("Raw Inputs:\n");
+				needs.forEach((k, v) -> sb.append(String.format("%s: %f%n", k, v)));
+				sb.append("\nProduction Steps:\nMachine%\tMachine\tRecipe\n");
+				steps.values().forEach(s -> {
+					sb.append(String.format("%.4f%%\t%s\t", s.count * 100, s.recipe.machine));
+					if (!s.recipe.name.isBlank())
+						sb.append(String.format("%s\t", s.recipe.name));
+					sb.append(String.format("(%s > %s)%n", s.recipe.ins.keySet(), s.recipe.outs.keySet()));
+				});
+				out.setText(sb.toString());
+			});
+			gbc.gridy = gbc.gridx = 0;
+			gbc.anchor = GridBagConstraints.BASELINE_TRAILING;
+			JPanel outPane = new JPanel(new GridBagLayout());
+			outPane.setBorder(new TitledBorder("Production Output"));
+			outPane.add(new JLabel("Item: "), gbc);
+			gbc.gridy = 1;
+			outPane.add(new JLabel("Rate (items/min): "), gbc);
+			gbc.gridy = 2;
+			gbc.gridwidth = 2;
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+			outPane.add(calculate, gbc);
+			gbc.gridy = 3;
+			gbc.weightx = gbc.weighty = 1;
+			gbc.fill = GridBagConstraints.BOTH;
+			outPane.add(new JScrollPane(out), gbc);
+			gbc.gridwidth = 1;
+			gbc.gridx = gbc.gridy = 1;
+			gbc.anchor = GridBagConstraints.BASELINE_LEADING;
+			gbc.weightx = gbc.weighty = 0;
+			outPane.add(spin, gbc);
+			gbc.gridy = 0;
+			outPane.add(itmSel, gbc);
+			frame.add(scroll, BorderLayout.LINE_START);
+			frame.add(outPane, BorderLayout.CENTER);
+			frame.pack();
+			frame.setSize(frame.getWidth(), Toolkit.getDefaultToolkit().getScreenSize().height);
+			frame.setVisible(true);
 		});
-		JTextArea out = new JTextArea(sb.toString());
-		out.setEditable(false);
-		JOptionPane.showMessageDialog(null, out);
+
+//		Scanner scan = new Scanner(Planner.class.getResourceAsStream("/best.tsv")).useDelimiter(DELIM);
+//		scan.nextLine();
+//		HashMap<String, Recipe> recipes = new HashMap<>();
+//		while (scan.hasNextLine())
+//			recipes.put(scan.next(),
+//					new Recipe(scan.next(), parseList(scan.next()), scan.next(), parseList(scan.next())));
+//		scan.close();
+//		JOptionPane.showMessageDialog(null, pane, "What is the desired item and rate?", JOptionPane.QUESTION_MESSAGE);
+//		System.out.printf("Raws: %s%n", raws);
+//		scan.close();
+//		JOptionPane.showMessageDialog(null, out);
 	}
 
 	private static final record Recipe(String name, HashMap<String, Float> ins, String machine,
 			HashMap<String, Float> outs) {
+
+		@Override
+		public String toString() {
+			return name.isBlank() ? "Default" : name;
+		}
 	}
 
 	private static final class ProductionStep {
